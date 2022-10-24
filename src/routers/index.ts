9a21682af7bfb95a520f4adeb2ff4ab1b4b7e665
Guilherme de Oliveira <smo.guilherme93@gmail.com>
2022-10-24 10:33:59 -0300

    fixed: missing fields on data validation schema for payment requisitions.

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -2,5 +2,6 @@ export * from './users-router';
 export * from './authentication-router';
 export * from './events-router';
 export * from './enrollments-router';
-export * from './tickets-router'
-export * from './accommodations-router'
+export * from './tickets-router';
+export * from './accommodations-router';
+export * from './payment-router';
