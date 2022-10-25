76cd0c1784add10330df744502b0f7c2bf3693f5
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-25 19:30:19 -0300

    feat: create route get /payments

--- src/routers/payment-router.ts
+++ src/routers/payment-router.ts
@@ -1,4 +1,4 @@
-import { confirmTicketPurchase } from '@/controllers';
+import { confirmTicketPurchase, findTicketPurchase } from '@/controllers';
 import { authenticateToken, validateBody } from '@/middlewares';
 import { purchaseInfo } from '@/schemas/ticket-purchase-schema';
 import { Router } from 'express';
@@ -7,5 +7,6 @@ const paymentRouter = Router();
 
 paymentRouter.all('/*', authenticateToken);
 paymentRouter.post('/', validateBody(purchaseInfo), confirmTicketPurchase);
+paymentRouter.get('/', findTicketPurchase);
 
 export { paymentRouter };
