7cb7fe1101b2701e969197cc83b88a2f9ad4b6a6
Yorí Galisteu <yorigcsdev@gmail.com>
2022-10-30 20:21:21 -0300

    feat: create reserveHotelRoom service

--- src/services/hotels-service/index.ts
+++ src/services/hotels-service/index.ts
@@ -1,13 +1,27 @@
+import { notFoundError } from '@/errors';
 import hotelRepository from '@/repositories/hotel-repository';
-import { Hotel } from '@prisma/client';
+import userTicketRepository from '@/repositories/payment-repository';
+import { Hotel, RoomUserTicket } from '@prisma/client';
 
 async function getAllHotels(): Promise<Hotel[]> {
   const hotels = await hotelRepository.findAll();
   return hotels;
 }
 
+async function reserveHotelRoom(userId: number, roomId: number): Promise<RoomUserTicket> {
+  const userTicket = await userTicketRepository.findUserTicketByUserId(userId);
+  if (!userTicket) throw notFoundError();
+
+  const room = await hotelRepository.findRoom(roomId);
+  if (!room) throw notFoundError();
+
+  const roomUserTicket = await hotelRepository.createRoomUserTicket(room.id, userTicket.id);
+  return roomUserTicket;
+}
+
 const hotelsService = {
   getAllHotels,
+  reserveHotelRoom,
 };
 
 export default hotelsService;
